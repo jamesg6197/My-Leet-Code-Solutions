db9e69d3b7531540769e5c9e2dd71b82b80dcb0e
jamesg6197 <34427740+jamesg6197@users.noreply.github.com>
2020-11-01 15:36:52 -0800

    Create leetcode.py

---
+++ leetcode.py
@@ -0,0 +1,14 @@
+// Leet code Solutions in Python
+def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        columns = []
+        for columnnum in range(n):
+            column = []
+            for row in matrix:
+                column.append(row[columnnum])
+            columns.append(column)
+        for rownum in range(n):
+            for element in range(n):
+                column = columns[rownum]
+                celement = column[(-1*element) - 1]
+                matrix[rownum][element] = celement
